50c24043cd0ed045d7230258b6726bd79acfa459
laboratorio <laboratorio@UPOAULA10712>
2025-05-14 08:29:50 -0500

    Agregar archivos de proyecto.

---
+++ PruebasAPI/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿namespace PruebasAPI.Models
+{
+    public class Country
+    {
+        public CountryName name { get; set; }
+        public Boolean independent { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+    }
+
+    public class CountryName
+    {
+        public string common { get; set; }
+        public string official { get; set; }
+    }
+}
